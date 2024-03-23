a0f2fcb6f6880131932c0f0f14e762827b943f20
JoaoAHaupt <joao.haupt@aluno.ifsp.edu.br>
2024-03-23 15:23:19 -0300

    lepo

--- 02-funcoes/exercicios_2/Ex05.py
+++ 02-funcoes/exercicios_2/Ex05.py
@@ -2,4 +2,17 @@
 Ex05 - Verificador de Palíndromos: Solicite ao usuário que digite uma palavra ou frase e 
 verifique se é um palíndromo (ou seja, pode ser lida de frente para trás e de trás para frente da mesma forma).
 '''
+import re
 
+# tirar a acentuação
+
+frase = input().lower().replace(' ', '')
+frase = re.sub(r'[^\w\s]', '', frase)
+reverse = ''.join(reversed(frase))
+
+if frase == reverse:
+    print('Palindromo')
+else:
+    print('normie')
+print(frase)
+print(reverse)
--- 02-funcoes/exercicios_2/contador-de-palavras.py
+++ 02-funcoes/exercicios_2/contador-de-palavras.py
@@ -1,10 +1,6 @@
-import re
 
 frase = input("Digite a frase: ").split(' ')
 
-padrao = re.compile(r'^[a-zA-Z]+$')
-
-frase = [palavra for palavra in frase if padrao.match(palavra)]
 dicionario = {}
 
 for i in range(0, len(frase), 1):
